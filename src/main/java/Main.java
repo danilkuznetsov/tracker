9b81d11e262ed3ec77062b93fd252f89f52388da
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2018-05-27 22:56:59 +0300

    implement aggregation reports logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,85 @@
-import model.Track;
 import services.TrackingServes;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        TrackingServes ts  = new TrackingServes();
-        Track track = ts.getTrackByIssueNumber(1099, "socialscore");
-        System.out.println(track);
-        System.out.println(track.getTotalTrack());
+        String apiKey = "Insert your private api key";
+        String owner = "vkuchyn";
+        String manager = "@JujaD";
+
+        TrackingServes ts = new TrackingServes(apiKey, owner, manager);
+
+
+        String repoName = "github-issue-stat";
+        String issueLabels = ""; // A list of comma separated label names. Example: bug,ui,@high
+        String issueState = "open";
+
+        List<Integer> numberIssues = ts.fetchIdIssues(repoName, issueState, issueLabels);
+
+        System.out.println("Found next issue: \n");
+        System.out.println(numberIssues);
+
+        Map<String, Integer> allTimeByAuthor = numberIssues
+                .stream()
+                .map((number) -> {
+                    try {
+                        return ts.getTrackByIssueNumber(number, repoName);
+                    } catch (IOException e) {
+                        throw new RuntimeException();
+                    }
+                })
+                .flatMap(track -> {
+                    List<TimeReport> reports = track.getTotalTrack()
+                            .entrySet()
+                            .stream()
+                            .map(en -> new TimeReport(en.getKey(), en.getValue()))
+                            .collect(Collectors.toList());
+
+                    System.out.println("Issue:  " + track.getIssueNumber());
+                    System.out.println("Issue Title:  " + track.getIssueTitle());
+                    System.out.println("\nTime by authors:\n");
+                    System.out.println(reports);
+                    System.out.println("-----------------------------------------------");
+
+                    return reports.stream();
+                })
+                .collect(
+                        Collectors.toMap(TimeReport::getAuthor, TimeReport::getTime, (d1, d2) -> d1 + d2)
+                );
+
+        System.out.println("\n Time track by author: \n");
+        System.out.println(allTimeByAuthor);
+
+    }
+
+    private static class TimeReport {
+        private String author;
+        private int time;
+
+        public TimeReport(String author, int time) {
+            this.author = author;
+            this.time = time;
+        }
+
+        public String getAuthor() {
+            return author;
+        }
+
+        public int getTime() {
+            return time;
+        }
+
+        @Override
+        public String toString() {
+            return "TimeReport{" +
+                    "author='" + author + '\'' +
+                    ", time=" + time +
+                    '}';
+        }
     }
 }
--- src/main/java/services/TrackingServes.java
+++ src/main/java/services/TrackingServes.java
@@ -8,27 +8,57 @@ import org.eclipse.egit.github.core.RepositoryId;
 import org.eclipse.egit.github.core.client.GitHubClient;
 import org.eclipse.egit.github.core.service.IssueService;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
-import java.util.*;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class TrackingServes {
     private final Pattern pattern = Pattern.compile("^[0-9HMhm]+");
     private final Pattern onlyDigit = Pattern.compile("^[0-9]+");
-    private final String BIZON4IK = "@Bizon4ik";
-    private String CREDENTIAL;
-    private final String OWNER_REPOSITORY = "vkuchyn";
+    private String manager;
+    private String credential;
+    private String ownerRepository;
 
     public TrackingServes() throws IOException {
         Properties prop = new Properties();
         InputStream input = this.getClass().getClassLoader().getResourceAsStream("config.properties");
         prop.load(input);
-        this.CREDENTIAL = prop.getProperty("credential");
+        this.credential = prop.getProperty("credential");
+        this.manager = "@Bizon4ik";
+        this.ownerRepository = "vkuchyn";
     }
+
+    public TrackingServes(String apiKey, String repoOwner, String manager) {
+        this.credential = apiKey;
+        this.manager = manager;
+        this.ownerRepository = repoOwner;
+    }
+
+    public List<Integer> fetchIdIssues(String repoName, String state, String labels) throws IOException {
+        IssueService issueService = new IssueService(getGitHubClient());
+        Map<String, String> params = new HashMap<>();
+        params.put("state", state);
+
+        if (!labels.isEmpty()) {
+            params.put("labels", labels);
+        }
+
+        return issueService.getIssues(getProvider(repoName), params)
+                .stream()
+                .map(Issue::getNumber)
+                .collect(Collectors.toList());
+    }
+
 
     public Track getTrackByIssueNumber(Integer issueNumber, String repoName) throws IOException {
         Issue issue = getIssueByNumber(getProvider(repoName), issueNumber);
@@ -47,50 +77,50 @@ public class TrackingServes {
     }
 
     private Map<Integer, Map<String, Integer>> findTrackInPullRequest(List<Issue> issuesList, Integer issueNumber, String repoName) {
-       Map<Integer, Map<String, Integer>> result = new HashMap<>();
-       issuesList.forEach(issue -> {
-                                     String[] words = issue.getTitle().replaceFirst("[-:_]", " ").split(" ");
-                                     if(words[0].charAt(0) == "#".charAt(0)){
-                                         words[0] = words[0].replaceFirst("[#]", "");
-                                     }
-                                     if (onlyDigit.matcher(words[0]).matches() && words[0].equals(issueNumber.toString())){
-                                         Map<Integer, Map<String, Integer>> bodyTrack = getTrackFromIssue(issue);
-                                         result.put(issue.getNumber(), bodyTrack.get(issue.getNumber()));
-                                         try {
-                                             Map<String, Integer> commentsTrack = getTrackFromIssueComments(issue.getNumber(),repoName);
-                                             if (result.get(issue.getNumber()) == null){
-                                                 result.put(issue.getNumber(), commentsTrack);
-                                             }else{
-                                                 sumUpTwoTrack(result.get(issue.getNumber()), commentsTrack);
-                                             }
-                                         } catch (IOException e) {
-                                             e.printStackTrace();
-                                         }
-                                     }
-                                });
+        Map<Integer, Map<String, Integer>> result = new HashMap<>();
+        issuesList.forEach(issue -> {
+            String[] words = issue.getTitle().replaceFirst("[-:_]", " ").split(" ");
+            if (words[0].charAt(0) == "#".charAt(0)) {
+                words[0] = words[0].replaceFirst("[#]", "");
+            }
+            if (onlyDigit.matcher(words[0]).matches() && words[0].equals(issueNumber.toString())) {
+                Map<Integer, Map<String, Integer>> bodyTrack = getTrackFromIssue(issue);
+                result.put(issue.getNumber(), bodyTrack.get(issue.getNumber()));
+                try {
+                    Map<String, Integer> commentsTrack = getTrackFromIssueComments(issue.getNumber(), repoName);
+                    if (result.get(issue.getNumber()) == null) {
+                        result.put(issue.getNumber(), commentsTrack);
+                    } else {
+                        sumUpTwoTrack(result.get(issue.getNumber()), commentsTrack);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
         return result;
     }
 
     private void sumUpTwoTrack(Map<String, Integer> currentTrack, Map<String, Integer> newTrack) {
-        newTrack.forEach((k,v)->{
-            if (currentTrack.get(k) == null){
+        newTrack.forEach((k, v) -> {
+            if (currentTrack.get(k) == null) {
                 currentTrack.put(k, v);
-            }else {
-                currentTrack.put(k, currentTrack.get(k)+v);
+            } else {
+                currentTrack.put(k, currentTrack.get(k) + v);
             }
         });
     }
 
     private Map<String, Integer> getTrackFromIssueComments(Integer issueNumber, String repoName) throws IOException {
         List<Comment> comments = getCommentsByIssueNumber(issueNumber, repoName);
-        Map<String, Integer>  commentsTrack = new HashMap<>();
-        for(Comment c: comments){
-            Map<String, Integer>  commentTime = getTimeFromComments(c);
-            if (commentTime.size() > 0){
-                for(Map.Entry<String, Integer> entity : commentTime.entrySet()){
-                    if (commentsTrack.get(entity.getKey())==null) {
+        Map<String, Integer> commentsTrack = new HashMap<>();
+        for (Comment c : comments) {
+            Map<String, Integer> commentTime = getTimeFromComments(c);
+            if (commentTime.size() > 0) {
+                for (Map.Entry<String, Integer> entity : commentTime.entrySet()) {
+                    if (commentsTrack.get(entity.getKey()) == null) {
                         commentsTrack.put(entity.getKey(), entity.getValue());
-                    }else {
+                    } else {
                         commentsTrack.put(entity.getKey(), commentsTrack.get(entity.getKey()) + entity.getValue());
                     }
                 }
@@ -100,28 +130,28 @@ public class TrackingServes {
     }
 
     private Map<String, Integer> getTimeFromComments(Comment c) {
-        int startIndex = c.getBody().indexOf(BIZON4IK);
-        Map<String, Integer>  result = new HashMap<>();
-        if (startIndex != -1){
+        int startIndex = c.getBody().indexOf(manager);
+        Map<String, Integer> result = new HashMap<>();
+        if (startIndex != -1) {
             result = parseCommet(c, startIndex);
         }
         return result;
     }
 
     private Map<String, Integer> parseCommet(Comment c, int startIndex) {
-        Map<String, Integer>  result = new HashMap<>();
-        if (startIndex == c.getBody().lastIndexOf(BIZON4IK)){
+        Map<String, Integer> result = new HashMap<>();
+        if (startIndex == c.getBody().lastIndexOf(manager)) {
             String[] st = c.getBody()
-                    .substring(startIndex+10, c.getBody().length() >= startIndex + 24 ? startIndex + 24 : c.getBody().length())
+                    .substring(startIndex + 10, c.getBody().length() >= startIndex + 24 ? startIndex + 24 : c.getBody().length())
                     .toLowerCase()
                     .replaceAll("\n", " ")
                     .split(" ");
 
-            for(String s :  st) {
-                if(pattern.matcher(s.trim()).matches()){
+            for (String s : st) {
+                if (pattern.matcher(s.trim()).matches()) {
                     int time = parseTime(s);
                     if (time > 0) {
-                        if(result.get(c.getUser().getLogin()) == null){
+                        if (result.get(c.getUser().getLogin()) == null) {
                             result.put(c.getUser().getLogin(), time);
                         } else {
                             result.put(c.getUser().getLogin(), result.get(c.getUser().getLogin()) + time);
@@ -139,12 +169,12 @@ public class TrackingServes {
     }
 
     private Map<Integer, Map<String, Integer>> getTrackFromIssue(Issue issue) {
-        Map<Integer, Map<String, Integer>>  result = new HashMap<>();
-        if (issue.getBody() != null){
-            int startIndex = issue.getBody().indexOf(BIZON4IK);
-            if (startIndex != -1){
+        Map<Integer, Map<String, Integer>> result = new HashMap<>();
+        if (issue.getBody() != null) {
+            int startIndex = issue.getBody().indexOf(manager);
+            if (startIndex != -1) {
                 Map<String, Integer> time = parseBody(issue, startIndex);
-                if(time.size()>0){
+                if (time.size() > 0) {
                     result.put(issue.getNumber(), time);
                 }
             }
@@ -153,16 +183,16 @@ public class TrackingServes {
         return result;
     }
 
-    private Map<String,Integer> parseBody(Issue issue, int startIndex) {
-        Map<String, Integer>  result = new HashMap<>();
-        if (startIndex == issue.getBody().lastIndexOf(BIZON4IK)){
+    private Map<String, Integer> parseBody(Issue issue, int startIndex) {
+        Map<String, Integer> result = new HashMap<>();
+        if (startIndex == issue.getBody().lastIndexOf(manager)) {
             String[] st = issue.getBody()
-                    .substring(startIndex+10, issue.getBody().length() >= startIndex + 24 ? startIndex + 24 : issue.getBody().length())
+                    .substring(startIndex + manager.length(), issue.getBody().length() >= startIndex + 24 ? startIndex + 24 : issue.getBody().length())
                     .toLowerCase()
                     .split(" ");
 
-            for(String s :  st) {
-                if(pattern.matcher(s).matches()){
+            for (String s : st) {
+                if (pattern.matcher(s).matches()) {
                     int time = parseTime(s);
                     if (time > 0) {
                         result.put(issue.getUser().getLogin(), time);
@@ -178,20 +208,20 @@ public class TrackingServes {
     private int parseTime(String s) {
         int h = 0;
         int m = 0;
-        int hourIndex  = s.indexOf("h");
-        if(hourIndex != -1){
+        int hourIndex = s.indexOf("h");
+        if (hourIndex != -1) {
             h = h + Integer.valueOf(s.substring(0, hourIndex));
             int minutIndex = s.indexOf("m");
-            if(minutIndex != -1){
-                m = Integer.valueOf(s.substring(hourIndex+1, minutIndex));
+            if (minutIndex != -1) {
+                m = Integer.valueOf(s.substring(hourIndex + 1, minutIndex));
             }
-        }else {
+        } else {
             int minutIndex = s.indexOf("m");
-            if(minutIndex != -1){
+            if (minutIndex != -1) {
                 m = Integer.valueOf(s.substring(0, minutIndex));
             }
         }
-        return 60*h+m;
+        return 60 * h + m;
     }
 
     private Issue getIssueByNumber(IRepositoryIdProvider provider, Integer issueNumber) throws IOException {
@@ -208,12 +238,12 @@ public class TrackingServes {
     }
 
     private IRepositoryIdProvider getProvider(String repoName) {
-        return new RepositoryId(OWNER_REPOSITORY, repoName);
+        return new RepositoryId(ownerRepository, repoName);
     }
 
     private GitHubClient getGitHubClient() {
         GitHubClient client = new GitHubClient();
-        client.setOAuth2Token(CREDENTIAL);
+        client.setOAuth2Token(credential);
         return client;
     }
 }
